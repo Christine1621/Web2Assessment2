d90e73399fe27634f24fbf7778a193b96bf15cfb
Christine1621 <350645256@qq.com>
2024-09-30 00:25:54 +0800

    follow commit

--- crowdfunding_api/crowdfunding_db.js
+++ crowdfunding_api/crowdfunding_db.js
@@ -1,5 +1,6 @@
+//Introducing the mysql module for interacting with MySQL databases
 const mysql = require('mysql');
-
+//Create a database connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -11,5 +12,5 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to the database!');
 });
-
+//Export database connection objects so that other modules can be imported.
 module.exports = connection;
--- crowdfunding_api/thefundraiser.js
+++ crowdfunding_api/thefundraiser.js
@@ -1,3 +1,4 @@
+//Introduce the required modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -5,11 +6,10 @@ const db = require('./crowdfunding_db');
 
 const server = express();
 
-//const PORT = 3000;
+server.use(cors());//Enable CORS
+server.use(bodyParser.json());//Parse the request body in JSON format
 
-server.use(cors());
-server.use(bodyParser.json());
-
+//Define an API to get all fundraisers
 server.get('/api/fundraisers', (req, res) => {
     const sql = 'SELECT * FROM FUNDRAISER';
     db.query(sql, (err, results) => {
@@ -20,6 +20,7 @@ server.get('/api/fundraisers', (req, res) => {
     });
 });
 
+//Define an API to get all categories
 server.get('/api/categories', (req, res) => {
     const sql = 'SELECT * FROM CATEGORY';
     db.query(sql, (err, results) => {
@@ -30,6 +31,7 @@ server.get('/api/categories', (req, res) => {
     });
 });
 
+//Define an API to get fundraising activities under a specific category based on the category ID
 server.get('/api/fundraisers/category/:categoryId', (req, res) => {
     const sql = `
         SELECT f.*, c.NAME as CATEGORY_NAME 
@@ -44,6 +46,7 @@ server.get('/api/fundraisers/category/:categoryId', (req, res) => {
     });
 });
 
+//Define an API to get a fundraiser ID
 server.get('/api/fundraiser/:id', (req, res) => {
     const sql = 'SELECT * FROM FUNDRAISER WHERE FUNDRAISER_ID = ?';
     db.query(sql, [req.params.id], (err, results) => {
@@ -54,9 +57,10 @@ server.get('/api/fundraiser/:id', (req, res) => {
     });
 });
 
-
+//Set the port of the server to 3000 in the environment variable
 const PORT = process.env.PORT || 3000;
 
+//Start the server and listen on the specified port
 server.listen(PORT, () => {
     console.log(`The server is running, port: ${PORT}`);
 });
